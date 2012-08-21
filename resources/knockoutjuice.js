a9fae65ee27c8c7472640f68ae052207fa9c67c5
Kyle Kirby <kylekirby@optimalconnection.net>
2012-08-21 18:26:22 -0500

    Initial Commit

---
+++ resources/knockoutjuice.js
@@ -0,0 +1,8 @@
+var Require = function(file){
+	document.write('<script type="text/javascript" src="' + file + '"></script>');
+}
+
+Require('resources/backend/joose.js');
+Require('resources/backend/knockout.js');
+Require('resources/backend/jQuery.js');
+Require('lib/Helper.js');
