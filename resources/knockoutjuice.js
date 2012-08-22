5f38baf172f349907e2c29bfea34cd1102018e9e
Kyle Kirby <kylekirby@optimalconnection.net>
2012-08-22 13:52:36 -0500

    Did some more organization along with other fun stuff.
    Made the meta attribute observable.Array be able to access KOs array methods via getters and setters.
    
    Did more organization. Moved a lot of files around.

--- resources/knockoutjuice.js
+++ resources/knockoutjuice.js
@@ -1,8 +1,17 @@
+/**
+ * We create a simple Require method. This will be
+ * overwritten by a more robust one inside of
+ * lib/Helper.js
+ */
 var Require = function(file){
 	document.write('<script type="text/javascript" src="' + file + '"></script>');
 }
 
+// We need Joose.
 Require('resources/backend/joose.js');
+// We need Knockout
 Require('resources/backend/knockout.js');
+// And lastly, we need jQuery.
 Require('resources/backend/jQuery.js');
+// Lets get our helper.
 Require('lib/Helper.js');
